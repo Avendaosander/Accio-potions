9642ea5e4fd835d587f0f1e9bca7fde28b857ac6
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-06-03 13:35:37 -0400

    Custoom Hook actualizado para realizar busquedas

--- src/hooks/usePotions.js
+++ src/hooks/usePotions.js
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react"
 import obtenerPociones from "../services/getPotions"
+import searchPotions from "../services/getSearch"
 
 function usePotions() {
    const [pociones, setPociones] = useState([])
    const [loading, setLoading] = useState(false)
    const [error, setError] = useState(null)
+   const [search, setSearch] = useState('')
 
    useEffect(() => {
       const getPotions = async() => {
          try {
             setLoading(true)
             setError(null)
-            const potions = await obtenerPociones()
+            const potions = search === '' ? await obtenerPociones() : await searchPotions(search)
             setPociones(potions === null ? [] : potions)
          } catch (error) {
             setError(error.message)
@@ -20,9 +22,9 @@ function usePotions() {
          }
       }
       getPotions()
-   }, [])
+   }, [search])
 
-   return ({pociones, setPociones, loading, error})
+   return ({pociones, setPociones, loading, error, setSearch})
 }
 
 export default usePotions
